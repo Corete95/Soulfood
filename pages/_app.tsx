27ee6cecc478cd0e4c80d2c55b9b6cf76c091e9e
Corete95 <fanthell95@gmail.com>
2023-07-05 21:36:54 +0900

    feat/구글애널리틱스 연결

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,28 @@ import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
 import { DefaultSeo } from 'next-seo';
 import SEO from '../seo.config';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <DefaultSeo {...SEO} />
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-91KC3BVREN`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-91KC3BVREN');
+          `,
+        }}
+      />
       <Component {...pageProps} />
     </>
   );

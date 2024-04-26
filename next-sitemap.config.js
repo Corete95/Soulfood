f0bffce9fd619a3612e4359bcac78d29c3538152
Corete95 <fanthell95@gmail.com>
2024-04-26 15:13:19 +0900

    fix/sitemap fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://next-map-dusky.vercel.app',
+  siteUrl: process.env.SITE_URL || 'https://soulfood-map.vercel.app/',
   generateRobotsTxt: true,
 };
